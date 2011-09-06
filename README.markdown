644720aad112003406838eab1521d1214f0fa7a0
Dave Newton <davelnewton@gmail.com>
2011-09-06 10:54:08 -0400

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,13 @@
+# Paperclip Playground
+
+Playing around with paperclip.
+
+Things I want to look at:
+
+* Multiple file uploads
+* Thumbnails
+* Watermarking
+* Amazon S3 storage
+* attachment_on_the_fly (not really paperclip)
+* :hash interpolation
+* Updating existing thumbnails on style changes
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperclipExample::Application.config.secret_token = '295f7cfd66c5ff4c5b0f42f2278ace1c519932068193323d2dbc7387e37dd70169340077f184fd78c6a5beab6fce825d41f632182051a0b271cf177f22864ce0'
